30f66b5eac36fb54b95a0f7aa71dc17224fb810b
NgomsoGaya <ngomso.gaya09@gmail.com>
2023-05-31 10:44:39 +0200

    pseudocode

---
+++ factory/factory.js
@@ -0,0 +1,19 @@
+function busTravel(){
+//The first function should take in a parameter and return nothing
+
+//The second function should check which destination is lelected
+//Then check what is the travel time is it peak or not
+//if it is a return trip calculate accordingly
+//if it is peak add a 25% increase on the normal price else do not
+
+//the third function should get the number of single tripscfrom the above function accordingly
+
+//the fourth one should get the price per single trip
+
+//the fifth one should get the number of return trips if it is a return trip
+
+//the sixth one should get the price per return trip if it is a return trip
+
+
+
+}
