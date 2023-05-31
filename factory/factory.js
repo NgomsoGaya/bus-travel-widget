fc850fdbdf33ab3a7092ddc31564ca7ba404e32b
NgomsoGaya <ngomso.gaya09@gmail.com>
2023-05-31 14:06:04 +0200

    a few test and some functionality

--- factory/factory.js
+++ factory/factory.js
@@ -1,16 +1,25 @@
 
 function busTravel(){
-    let cost = 0;
+    let costs = {
+        'Khayelitsha': 40, 
+        'Dunoon': 25, 
+        'MitchellsPlain': 30, 
+    };
+
+    let noOf1Trips = 0;
+
+    let noOf2Trips = 0;
+
     let globalVariable1;
 //The first function should take in a parameter and return nothing
 function enterPoints(points){
-    if(points){
+    if(points && !isNaN(points)){
     points
     }
-    else if(!points){
+    else if(!points || isNaN(points)){
         points = 0
     }
-    
+
     globalVariable1 = points
 }
 
@@ -22,43 +31,76 @@ function getEnteredPoints(){
 //Then check what is the travel time is it peak or not
 //if it is a return trip calculate accordingly
 //if it is peak add a 25% increase on the normal price else do not
-function calculateTrips(location){
-    if(peak){
-        if(location == "Khayelitsha"){
-           //cost = 40 += (25/100)*40
-        }
-        else if(location == "Dunoon"){
-           //cost = 40 += (25/100)*25
-        }
-        else if(location == "M/Plain"){
-           //cost = 40 += (25/100)*30
-        }
+function calculateTrips(radio, town){
+    if(radio == 'Peak'){
+    for (var key in costs) {
+        if(key == "Khayelitsha" && town == "Khayelitsha"){
+            costs[key] += (25/100)*40
+           noOf1Trips = globalVariable1 / costs[key]
+           noOf2Trips = noOf1Trips/2
+         }
+         else if(key == "Dunoon" && town == "Dunoon"){
+            costs[key] += (25/100)*25
+           noOf1Trips = globalVariable1 / costs[key]
+           noOf2Trips = noOf1Trips/2
+         }
+         else if(key == "MitchellsPlain" && town == "MitchellsPlain"){
+            costs[key] += (25/100)*30
+           noOf1Trips = globalVariable1 / costs[key]
+           noOf2Trips = noOf1Trips/2
+         }
+    }   
     }
-    else if(off-peak){
-            if(location == "Khayelitsha"){
-               cost = 40
+
+    else if(radio == 'Off-peak'){
+        for (var keys in costs){
+            if(keys == "Khayelitsha" && town == "Khayelitsha"){
+              noOf1Trips = globalVariable1 / costs[keys]
+              noOf2Trips = noOf1Trips/2
             }
-            else if(location == "Dunoon"){
-               cost = 40
+            else if(keys == "Dunoon" && town == "Dunoon"){
+              noOf1Trips = globalVariable1 / costs[keys]
+              noOf2Trips = noOf1Trips/2
             }
-            else if(location == "Mitchells Plain"){
-               cost = 40
+            else if(keys == "MitchellsPlain" && town == "MitchellsPlain"){
+              noOf1Trips = globalVariable1 / costs[keys]
+              noOf2Trips = noOf1Trips/2
             }
+        }
+            
     }
 }
 //the third function should get the number of single tripscfrom the above function accordingly
-
+function getNumberOfSingleTrips(){
+        if(noOf1Trips > 1){
+         noOf1Trips = Math.round(noOf1Trips)
+        }
+        else if(noOf1Trips< 1){
+         noOf1Trips = 0
+        }
+    return noOf1Trips
+}
 //the fourth one should get the price per single trip
 
 //the fifth one should get the number of return trips if it is a return trip
-
+function getNumberOfReturnTrips(){
+    if(noOf2Trips > 1){
+     noOf2Trips = Math.round(noOf1Trips)
+    }
+    else if(noOf2Trips< 1){
+     noOf2Trips = 0
+    }
+return noOf2Trips
+}
 //the sixth one should get the price per return trip if it is a return trip
 
 
 return {
     enterPoints,
     getEnteredPoints,
-    calculateTrips
+    calculateTrips,
+    getNumberOfSingleTrips,
+    getNumberOfReturnTrips
 }
 
 }
--- test/factory.tests.js
+++ test/factory.tests.js
@@ -1,6 +1,7 @@
 describe("The Bus Travel Widget", function () {
 
-    it("It should return 0 if no points have been added yet", function () {
+    describe("Testing the input", function () {
+    it("It should return 0 points if no points have been added yet", function () {
 
        let bustravel = busTravel()
 
@@ -8,8 +9,7 @@ describe("The Bus Travel Widget", function () {
 
         assert.equal(0, bustravel.getEnteredPoints());
     })
-
-    it("It should return 100 if 100 points have been added", function () {
+    it("It should return 100 points if 100 points have been added", function () {
 
         let bustravel = busTravel()
  
@@ -18,7 +18,7 @@ describe("The Bus Travel Widget", function () {
          assert.equal(100, bustravel.getEnteredPoints());
      })
 
-     it("It should return 0 if points added are not a numer", function () {
+     it("It should return 0 points if points added are not a number", function () {
 
         let bustravel = busTravel()
  
@@ -26,7 +26,98 @@ describe("The Bus Travel Widget", function () {
  
          assert.equal(0, bustravel.getEnteredPoints());
      })
+    })
 
+    describe("Testing not enough points for single trips", function () {
+     it("It should return 0 trips if points added are not enough for a trip", function () {
 
+        let bustravel = busTravel()
+        
+        bustravel.enterPoints("30")
+        bustravel.calculateTrips("Off-peak", 'Khayelitsha')
+         assert.equal(0, bustravel.getNumberOfSingleTrips());
+     })
+
+     it("It should return 0 trips if points added are not enough for a trip", function () {
+
+        let bustravel = busTravel()
+        
+        bustravel.enterPoints("20")
+        bustravel.calculateTrips("Off-peak", 'Dunoon')
+         assert.equal(0, bustravel.getNumberOfSingleTrips());
+     })
+
+     it("It should return 0 trips if points added are not enough for a trip", function () {
+
+        let bustravel = busTravel()
+ 
+        bustravel.enterPoints("25")
+        bustravel.calculateTrips("Off-peak", 'MitchellsPlain')
+         assert.equal(0, bustravel.getNumberOfSingleTrips())
+        
+     })
+    })
+
+    describe("Testing for single trips per points", function () {
+     it("It should return 4 if points added are equivalent to 4 trips", function () {
+
+        let bustravel = busTravel()
+ 
+        bustravel.enterPoints("120")
+        bustravel.calculateTrips("Off-peak", 'MitchellsPlain')
+         assert.equal(4, bustravel.getNumberOfSingleTrips())
+        
+     })
+
+     it("It should return 3 if points added are equivalent to 3 trips", function () {
+
+        let bustravel = busTravel()
+ 
+        bustravel.enterPoints("75")
+        bustravel.calculateTrips("Off-peak", 'Dunoon')
+         assert.equal(3, bustravel.getNumberOfSingleTrips())
+        
+     })
+     it("It should return 1 if points added are equivalent to 1 trip", function () {
+
+        let bustravel = busTravel()
+ 
+        bustravel.enterPoints("50")
+        bustravel.calculateTrips("Off-peak", 'Khayelitsha')
+         assert.equal(1, bustravel.getNumberOfSingleTrips())
+        
+     })
+    })
+
+    describe("Testing for not enough points for a return trip", function () {
+     it("It should return 3 if points added are equivalent to 3 trips", function () {
+
+        let bustravel = busTravel()
+ 
+        bustravel.enterPoints("120")
+        bustravel.calculateTrips("Off-peak", 'MitchellsPlain')
+         assert.equal(0, bustravel.getNumberOfSingleTrips())
+        
+     })
+
+     it("It should return 4 if points added are equivalent to 4 trips", function () {
+
+        let bustravel = busTravel()
+ 
+        bustravel.enterPoints("100")
+        bustravel.calculateTrips("Off-peak", 'Dunoon')
+         assert.equal(0, bustravel.getNumberOfSingleTrips())
+        
+     })
+     it("It should return 4 if points added are equivalent to 4 trips", function () {
+
+        let bustravel = busTravel()
+ 
+        bustravel.enterPoints("100")
+        bustravel.calculateTrips("Off-peak", 'Dunoon')
+         assert.equal(0, bustravel.getNumberOfSingleTrips())
+        
+     })
+    })
 
 })
